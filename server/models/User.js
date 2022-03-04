811a0ec119e78a8da1b9056f3dc9c7c10c9a82ed
Benjamin Kim <bennykim523@gmail.com>
2022-03-04 13:50:18 -0500

    added users

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
+const Quiz = require('./Quiz');
 
-
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+})
 
 const User = mongoose.model('User', userSchema);
 
