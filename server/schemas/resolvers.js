d3179113a15f2a5acfe52eb633914057d7d8ff8a
Benjamin Kim <bennykim523@gmail.com>
2022-03-06 17:22:29 -0500

    added query/mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,64 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Quiz, Question } = require('../models');
-const { signToekn } = require('../utils/auth');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        users: async (parent, args, context) => {
+            return User.find();
+        },
+        quizzes: async (parent, args, context) => {
+            return Quiz.find();
+        },
+        questions: async (parent, args, context) => {
+            return Question.find();
+        }
     },
     Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return{ token, user };
+        },
+
+        addQuiz: async (parent, args, context) => {
+            const quiz = await Quiz.create(args);
+            await User.findByIdAndUpdate(context.user.id, {
+                $push: { quizzes: quiz },
+            });
+
+            return quiz;
+        },
+
+        addQuestion: async (parent, args, context) => {
+            console.log(args);
+            console.log(context.quiz);
+            const question = await Question.create(args);
+            await Quiz.findByIdAndUpdate(context.quiz.id, {
+                $push: { questions: question },
+            })
+
+            return question;
+        },
+
+        login: async (parent, { username, password }) => {
+            const user = await User.findOne({ username });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
     },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,15 +2,24 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
-
+        _id: ID!
+        username: String!
+        email: String!
+        quizzes: [Quiz]
     }
 
     type Quiz {
-
+        _id: ID!
+        name: String!
+        category: String!
+        question: [Question]
     }
     
     type Question {
-
+        _id: ID!
+        title: String!
+        answer: String!
+        choices: [String]!
     }
 
     type Auth {
@@ -19,11 +28,31 @@ const typeDefs = gql`
     }
 
     type Query {
-
+        users: [User]
+        user(username: String!): User
+        quizzes: [Quiz]
+        questions: [Question]
     }
 
     type Mutation {
-
+        addUser(
+            username: String!
+            email: String!
+            password: String!
+        ) : Auth
+        addQuiz(
+            name: String!
+            category: String!
+        ) : Quiz
+        addQuestion(
+            title: String!
+            answer: String!
+            choices: [String]!
+        ) : Question
+        login(
+            username: String!
+            password: String!
+        ): Auth
     }
 `;
 
