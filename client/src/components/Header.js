079ac20723067ef7f8643ed2ddaad22a0cbd4cfb
Lanay Brown <lanayb.03@gmail.com>
2022-03-07 13:51:14 -0500

    saving

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+
+const styles = {
+    navbar: {
+        background: 'orange',
+        textDecoration: 'none',
+    
+    }
+};
+
+
 function Navbar() {
     return (
         <div>
-            <nav>
+            <nav style={styles.navbar}>
                 <ul class='flex justify-end space-x-4 p-4 '>
                     <li><a href="/#create">Create</a></li>
                     <li><a href="/#login">Login</a></li>
