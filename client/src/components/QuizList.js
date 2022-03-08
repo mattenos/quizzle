210c7101be42c1baccf5cb94ae1bc5e591b48f37
Lanay Brown <lanayb.03@gmail.com>
2022-03-08 13:18:27 -0500

    saving changes

--- client/src/components/QuizList.js
+++ client/src/components/QuizList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/quizList.css';
 
 const QuizList = ({
     quizzes,
@@ -15,13 +16,13 @@ const QuizList = ({
 
     return (
         <div>
-            this will be a quiz list
+            {/* this will be a quiz list */}
             {quizzes.map((quiz) => (
                 <div key={quiz._id} className='card mb-3'>
                     <div className='card-body bg-light p-2'>
                         <h4 className='card-header bg-primary text-light p-2 m-0'>Title:{quiz.name} Category:{quiz.category}</h4>
                         <Link
-                        className='btn'
+                        id="take-quiz" className='btn'
                         to={`/quiz/${quiz._id}`}
                         >
                         Take this quiz
