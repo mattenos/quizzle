8f8ecd56cb04d885364ee613e26d7ac28a68f2ce
Benjamin Kim <bennykim523@gmail.com>
2022-03-08 14:33:22 -0500

    added username

--- client/src/components/QuizList.js
+++ client/src/components/QuizList.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/quizList.css';
 
-const QuizList = ({
-    quizzes,
-    name,
-    category,
-    author,
-}) => {
+const QuizList = ({ quizzes }) => {
     console.log(quizzes);
 
     if (!quizzes.length) {
@@ -20,7 +15,8 @@ const QuizList = ({
                 <div key={quiz._id} className='card border'>
                     <div className='card-body bg-light'>
                         <h4 className='card-header bg-primary text-light'>Title: {quiz.name} <br></br>
-                            Category: {quiz.category}</h4>
+                            Category: {quiz.category} <br></br>
+                            Created by: {quiz.author}</h4>
                         <Link
                         id="take-quiz" className='btn'
                         to={`/quiz/${quiz._id}`}
